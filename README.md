34ef374dbea2c114b0eeb81e0cdce6d753c3fb6d
rejincusi <rejincusi@gmail.com>
2019-09-25 21:27:09 +0200

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# melodify
+# todos
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="container">
+      <Todos />
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+import Todos from "./components/Todos";
+
+export default {
+  name: "app",
+  components: {
+    Todos
+  }
+};
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-display: "Montserrat", sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
 }
 </style>
